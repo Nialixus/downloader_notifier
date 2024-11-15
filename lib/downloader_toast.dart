82d6c91667ea3caa85cfed001273b3686d756086
Nialixus <nialixus@gmail.com>
2024-11-15 20:42:06 +0700

    [1.0.0] Initial Commit

---
+++ lib/downloader_toast.dart
@@ -0,0 +1,5 @@
+library downloader_toast;
+
+import 'package:flutter/material.dart';
+
+part 'src/views/downloader_list_page.dart';
---
+++ lib/src/views/downloader_list_page.dart
@@ -0,0 +1,12 @@
+part of '../../downloader_toast.dart';
+
+class DownloaderToastPage extends StatelessWidget {
+  const DownloaderToastPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+    );
+  }
+}
